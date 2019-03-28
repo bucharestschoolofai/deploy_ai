0358bad5c71bf9b9a85bb464659441d8263c65a1
paubric <paubric@gmail.com>
2019-03-28 22:23:37 +0200

    Update api.py

--- API/api.py
+++ API/api.py
@@ -1,4 +1,5 @@
 from keras.applications.mobilenet import MobileNet
+from keras import backend as K
 import tensorflow as tf
 from flask import Flask
 from flask import request
@@ -8,14 +9,14 @@ from sklearn.linear_model import LinearRegression
 app = Flask(__name__)
 
 # Load MobileNet
-MobileNet = MobileNet()
+mobile_net = MobileNet()
 
 # The following line is necessary because Flask runs on a different thread than the loaded model
 graph = tf.get_default_graph()
 
 # Load the deep convolutional recurrent extra crispy one neuron network
-LinearRegression = LinearRegression()
-LinearRegression.fit([[1], [2]], [[2], [4]])
+linear_regression = LinearRegression()
+linear_regression.fit([[1], [2]], [[2], [4]])
 
 
 @app.route('/hello')
@@ -32,7 +33,7 @@ def predict_regression():
     X = [[X]]
 
     # Predict
-    prediction = LinearRegression.predict(X)
+    prediction = linear_regression.predict(X)
 
     # Return prediction
     return str(prediction)
@@ -61,7 +62,40 @@ def predict_mobilenet():
     # The following line is necessary because Flask runs on a different thread than the loaded model
     with graph.as_default():
         # Predict
-        prediction = MobileNet.predict(image)
+        prediction = mobile_net.predict(image)
+
+    # Return prediction
+    return str(prediction)
+
+
+@app.route('/predict_mobilenet_slow', methods=['POST'])
+def predict_mobilenet_slow():
+    K.clear_session()
+    mobile_net = MobileNet()
+    graph = tf.get_default_graph()
+
+    # Get file descriptor
+    file = request.files['input']
+
+    # Read bytestream from file
+    string = file.read()
+
+    # Turn bytestream into 1D array
+    data = np.fromstring(string, dtype='uint8')
+
+    # Turn 1D array into 2D image
+    image = cv2.imdecode(data, cv2.IMREAD_COLOR)
+
+    # Resize image so it fits model input
+    image = cv2.resize(image, (224, 224))
+
+    # Reshape image so it fits model input
+    image = np.reshape(image, (1, *image.shape))
+
+    # The following line is necessary because Flask runs on a different thread than the loaded model
+    with graph.as_default():
+        # Predict
+        prediction = mobile_net.predict(image)
 
     # Return prediction
     return str(prediction)
